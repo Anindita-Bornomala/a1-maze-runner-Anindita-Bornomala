a439e30ace5226766b653d14255ac1793bc05396
Anindita Bornomala <aninborno@LAPTOP>
2024-01-16 20:13:22 -0500

    Updated -i with inputFilePath

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -2,6 +2,7 @@ package ca.mcmaster.se2aa4.mazerunner;
 
 import java.io.BufferedReader;
 import java.io.File;
+import org.apache.commons.cli.*; // EDIT 1
 import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,9 +13,30 @@ public class Main {
 
     public static void main(String[] args) {
         logger.info("** Starting Maze Runner");
+
+        // Edit 2
+        Options options = new Options();
+        options.addOption("i", "input", true, "Input file path");
+        
+        CommandLineParser parser = new DefaultParser();
+        // end of edit 2
+
         try {
-            logger.info("**** Reading the maze from file " + "-i");
-            BufferedReader reader = new BufferedReader(new FileReader("-i"));
+            // edit 3
+            
+            CommandLine cmd = parser.parse(options, args);
+
+            String inputFilePath;
+            if (cmd.hasOption("i")) {
+                inputFilePath = cmd.getOptionValue("i");
+            } else {
+                logger.error("Input file path is required");
+                return;
+            }
+            // end of edit 3
+            
+            logger.info("**** Reading the maze from file " + inputFilePath); // edit 4, from "-i" to inputFilePath
+            BufferedReader reader = new BufferedReader(new FileReader(inputFilePath)); // edit 5
             String line;
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
