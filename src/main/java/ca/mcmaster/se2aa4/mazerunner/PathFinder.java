f35c77e672af90bbe42b5e2fc3531f51d4969de7
Anindita Bornomala <aninborno@LAPTOP>
2024-01-21 01:10:49 -0500

    Added new class PathFinder, added methods

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/PathFinder.java
@@ -0,0 +1,21 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class PathFinder {
+
+    public String pathStart(String[][] mazeData) {
+        // given maze data, return start condition
+        return "False";
+    }
+
+    public String pathEnd(String[][] mazeData) {
+        // given maze data, return end condition
+        return "False";
+    }
+
+    public String currentStep() { return "False"; }
+
+    public String nextStep() { return "False"; }
+    
+
+    
+}
