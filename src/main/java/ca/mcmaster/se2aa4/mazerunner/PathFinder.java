6cbe8216657a4c6f2ae0b5ed98f0e9d0eaf435bb
Anindita Bornomala <aninborno@LAPTOP>
2024-01-21 01:12:38 -0500

    Moved rightHandRule method to PathFinder class

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/PathFinder.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/PathFinder.java
@@ -15,7 +15,19 @@ public class PathFinder {
     public String currentStep() { return "False"; }
 
     public String nextStep() { return "False"; }
-    
+
+    //pathfinding algorithm
+    public String rightHandRule() {
+        // get the maze as a matrix
+        // start at the "PASS" on column 1 (ex. r3, c1)
+        // Search left: one row up, same column
+        // Search right: one row down, same column
+        // Go forward: same row, one column forward 
+        // Mission: either go left, or go right, else go forward
+        // Straight line: Search for pass in next column (ex. r3) 
+        return "No path found!";
+    }
+
 
     
 }
