6587fcfe379ca66e2915879a351048851ebfaedf
Anindita Bornomala <aninborno@LAPTOP>
2024-01-21 01:15:42 -0500

    Added traverseMaze() method in PathSequence class

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/PathSequence.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/PathSequence.java
@@ -11,18 +11,8 @@ public class PathSequence {
         // return String[][] mazeData
         return "Empty maze!";
     }
-    
-    //pathfinding algorithm
-    public String rightHandRule() {
-        // get the maze as a matrix
-        // start at the "PASS" on column 1 (ex. r3, c1)
-        // Search left: one row up, same column
-        // Search right: one row down, same column
-        // Go forward: same row, one column forward 
-        // Mission: either go left, or go right, else go forward
-        // Straight line: Search for pass in next column (ex. r3) 
-        return "No path found!";
-    }
+
+    public String traverseMaze() { return "False"; }
 
     // Path guess checker
     public String pathCheck() {
