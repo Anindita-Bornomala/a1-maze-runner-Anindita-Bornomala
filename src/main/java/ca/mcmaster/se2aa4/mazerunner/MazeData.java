a670923214156da4e9c0ba0c81fd3267a9fc197a
Anindita Bornomala <aninborno@LAPTOP>
2024-01-21 03:14:12 -0500

    Edited reader in MazeData class

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeData.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeData.java
@@ -7,15 +7,15 @@ import java.io.IOException;
 
 public class MazeData {
 
-    public static char[][] storeMazeData(String filePath) throws IOException, FileNotFoundException{
+    public char[][] storeMazeData(String filePath) throws IOException, FileNotFoundException{
         
         // initialize an empty matrix with correct rows and columns
         int sumRows = 0;
         int sumCols = 0;
 
-        BufferedReader reader2 = new BufferedReader(new FileReader(filePath));
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
         String line2;
-        while ((line2 = reader2.readLine()) != null) {
+        while ((line2 = reader.readLine()) != null) {
             sumRows++;
             if (sumCols == 0) {
                 sumCols = line2.length();
@@ -26,10 +26,10 @@ public class MazeData {
 
 
         // Fill mazeData with proper data
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        BufferedReader reader2 = new BufferedReader(new FileReader(filePath));
         String line3;
         int row = 0;
-        while ((line3 = reader.readLine()) != null) {
+        while ((line3 = reader2.readLine()) != null) {
             for (int i = 0; i < line3.length(); i++) {
                 if (line3.charAt(i) == '#') {
                     mazeData[row][i] = 'W';
