133c3737afe58ffba271560c57d776d82edfd638
Anindita Bornomala <aninborno@LAPTOP>
2024-01-23 12:03:24 -0500

    Moved RightHandRule method to PathSequence class

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/PathSequence.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/PathSequence.java
@@ -5,19 +5,39 @@ public class PathSequence {
     public static String FILEPATH = "i";
     public static String PATHCHECK = "p";
 
-    public String getMaze() {
-        // get the file .txt
-        // convert into a matrix, so each word is an element (binary)
-        // return String[][] mazeData
-        return "Empty maze!";
-    }
+    public String findPath(char[][] mazeData) {
+        PathFinder pathFind = new PathFinder();
 
-    public String findPath() {
+        Integer[] startCond = pathFind.pathStart(mazeData);
+        Integer[] endCond = pathFind.pathEnd(mazeData);
+        Integer[] pointer = startCond; // THIS IS THE PLAYER
+        char direction = 'E';
+        String sequence = "";
+        Integer[] nextPosition = pathFind.nextStep(pointer, direction);
+        System.out.println();        
         
+        while (pointer[1] < endCond[1]) {
+            if (pathFind.checkRight(mazeData, pointer, direction) == false) {
+                if (pathFind.checkFront(mazeData, pointer, direction) == true) {
+                    pointer = pathFind.moveForward(pointer, nextPosition);
+                    sequence.concat("F");
+                } else {
+                    nextPosition = pathFind.nextStep(pointer, pathFind.turnRight(direction));
+                    sequence.concat("R");
+                }
+            } else {
+                nextPosition = pathFind.nextStep(pointer, pathFind.turnRight(direction));
+                sequence.concat("R");
+                pointer = pathFind.moveForward(pointer, nextPosition);
+                sequence.concat("F");
+            }
+        }
+        System.out.println("Final position: row " + pointer[0] + ", column " + pointer[1]);
+        return sequence;
+
         // RightHandRule pathFind = new RightHandRule();
         // use to get a canonical path string
-
-        return "False"; }
+        }
 
     public String factorize(String canonical) { return "False"; }
 
