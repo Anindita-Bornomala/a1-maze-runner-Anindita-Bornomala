c8f5b8524b083be7eecfc408e79b2974d90ee0a2
Anindita Bornomala <aninborno@LAPTOP>
2024-01-28 21:50:27 -0500

    Changed Main class to interface

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -3,14 +3,13 @@ package ca.mcmaster.se2aa4.mazerunner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Main {
-    private static final Logger logger = LogManager.getLogger();
-
+public interface Main {
+    static final Logger logger = LogManager.getLogger();
     public static void main(String[] args) {
         try {
             new MazeRunner(args);
         } catch (Exception e) {
             logger.error("/!\\ An error has occured /!\\");
         }
-    }
-}
+    }    
+} 
